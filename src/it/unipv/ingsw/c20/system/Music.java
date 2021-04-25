a41f1981750ba57f15798a26b1264ae7bbe6269d
MattiaSeravalli <mattia.seravalli01@universitadipavia.it>
2021-04-25 18:04:28 +0200

    Commenti a Music

--- src/it/unipv/ingsw/c20/system/Music.java
+++ src/it/unipv/ingsw/c20/system/Music.java
@@ -21,7 +21,7 @@ public class Music implements Runnable{
 	private int loop;
 	
 	/**
-	 * Music's constructor, it will starts the music and the thread that is going to handle it
+	 * Music's constructor, it will start the music and the thread that is going to handle it
 	 * @param musicFileName location of the music
 	 * @param loop loop of the song
 	 */
@@ -29,17 +29,18 @@ public class Music implements Runnable{
 		
 		this.loop = loop;
 	    try {
+	    	
+	    	//Open the input stream with the clip
 	    	this.clip = AudioSystem.getClip();
 			this.clip.open(AudioSystem.getAudioInputStream(new File(musicFileName)));
 			
+			//With gainControl we will control the music volume.
 			FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-			
-			gainControl.setValue(-20.0f); // Reduce volume by 10 decibels.
+			gainControl.setValue(-20.0f); 		//Reduces volume by 10 decibels.
 			
 			this.clip.loop(loop);
 			
 		} catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	    
@@ -61,15 +62,17 @@ public class Music implements Runnable{
 						this.clip.loop(this.loop);
 					}
 					if(this.clip.getFramePosition() == this.clip.getFrameLength()){
+						//Close the stream if the music has come to end.
 						this.clip.close();
 						return;
 					}
 				}
-				
 			}else{
 				if(this.loop == Clip.LOOP_CONTINUOUSLY){
+					//Stop the thread.
 					this.clip.stop();	
 				}else{
+					//Close the stream.
 					this.clip.close();
 					return;
 				}
